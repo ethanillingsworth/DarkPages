7d685a3916444076acad378f2df98b1874f7ce73
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2024-12-14 18:01:28 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# DarkPagesMD
+
+A javascript library that turns the default README.md viewer of Github Pages into a dark version.
+
+## Setup
+
+To use DarkPagesMD just import the javascript and css files into a blank html documents
+
+```html 
+<script type="module" src="https://ethanillingsworth.github.io/DarkPagesMD/dark.js"></script>
+<link rel="stylesheet" href="https://ethanillingsworth.github.io/DarkPagesMD/dark.css">
+```
---
+++ dark.js
@@ -0,0 +1,30 @@
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+
+import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/es/highlight.min.js';
+
+const content = document.createElement("div")
+content.id = "content"
+
+document.body.append(content)
+
+function giveHeadingIds(headingType) {
+    document.querySelectorAll(headingType).forEach((h) => {
+        h.id = h.innerText.replaceAll(" ", "-").replaceAll("'", "").toLowerCase()
+    })
+}
+
+fetch("../README.md")
+    .then(async (res) => {
+        content.innerHTML = marked.parse(await res.text())
+
+        document.title = document.querySelector("h1").innerText
+
+        const headingTypes = ["h1", "h2", "h3", "h4", "h5", "h6"]
+
+        headingTypes.forEach((heading) => {
+            giveHeadingIds(heading)
+        })
+
+        hljs.highlightAll();
+
+    }) 
