b4584eef5f8cc787018862e884e7d41ad180f457
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2025-03-12 17:36:46 -0500

    fix links

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import { DarkPages } from "./dark.js";
 DarkPages.init()
 
 
-DarkPages.addTab("DarkPages", "../")
+DarkPages.addTab("DarkPages", "./")
 
-DarkPages.addTab("Another MD File", "../Another.html")
+DarkPages.addTab("Another MD File", "./Another.md")
