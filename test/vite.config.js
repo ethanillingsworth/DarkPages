4e4166d8d8e3466f8e9ee33838ab5407c468cc95
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2026-04-18 12:25:44 -0500

    see if this fixes react conflicts

--- test/vite.config.js
+++ test/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	base: "/DarkPages/",
+	resolve: {
+		alias: {
+			// This forces Vite to always use the React version in your test/node_modules
+			// eslint-disable-next-line no-undef
+			react: path.resolve(__dirname, "./node_modules/react"),
+			// eslint-disable-next-line no-undef
+			"react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+		},
+	},
 });
